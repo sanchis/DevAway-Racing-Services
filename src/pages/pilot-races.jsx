86a5f914d10b04c5601ffab053995ba862d46dbb
sanchis <sanchis1992@gmail.com>
2021-01-24 20:27:55 +0100

    fix: key pilot races

--- src/pages/pilot-races.jsx
+++ src/pages/pilot-races.jsx
@@ -5,17 +5,20 @@ import Race from 'components/race'
 
 const pilots = getGlobalRanking();
 export default function PilotRaces() {
+
+    const renderRaces = (pilot) =>
+        getRankingByPilot(pilot.name).races.map((race, index) =>
+            <Race name={`Race ${index + 1}`} key={pilot.name + index} race={race} hightLightPilot={pilot.name} ></Race>
+        );
+
     return (
         <>
             <h1 className="is-size-1">Pilot Ranking</h1>
-            {pilots.map((pilot, positon) =>
-                <>
-                    <Pilot key={pilot.name} position={positon + 1} pilot={pilot} ></Pilot>
-                    {getRankingByPilot(pilot.name).races.map((race, index) =>
-                        <Race name={`Race ${index + 1}`} key={pilot.name + index} race={race} hightLightPilot={pilot.name} ></Race>
-                    )}
-
-                </>
+            {pilots.map((pilot, position) =>
+                <div key={pilot.name + position}>
+                    <Pilot position={position + 1} pilot={pilot} ></Pilot>
+                    {renderRaces(pilot)}
+                </div>
             )}
         </>
     );
